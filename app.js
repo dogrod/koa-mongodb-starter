977124f3cca0d87980a8d9eacd0ba9f4f0eb9344
zhulei <zhulei@xkeshi.com>
2017-07-13 22:53:36 +0800

    feat: use koa-bodyparser to resolve post body

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const Koa = require('koa')
 
 // koa-router returns a function
 const router = require('koa-router')() // IIFE!!!! important
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
+
 
 // log request url
 app.use(async (ctx, next) => {
@@ -17,10 +19,29 @@ router.get('/hello/:name', async (ctx, next) => {
   ctx.response.body = `<h1>Hello, ${name}</h1>`
 })
 
+// render page
 router.get('/', async (ctx, next) => {
-  ctx.response.body = `<h1>I'm Index</h1>`
+  ctx.response.body = `<h1>Index</h2>
+    <form action="/signin" method="post">
+      <p>Name: <input name="name" value="koa"></p>
+      <p>Password: <input name="password" value="password"></p>
+      <p><input type="submit" value="Submit"></p>
+    </form>`
 })
 
+router.post('/signin', async (ctx, next) => {
+  const name = ctx.request.body.name || ''
+  const password = ctx.request.body.password || ''
+
+  console.log(`signin with name: ${name}, password: ${password}`)
+  if (name && password) {
+    ctx.response.body = `<h1>Welcome, ${name}</h1>`
+  } else {
+    ctx.response.body = `<h1>Login Failed!</h1>`
+  }
+})
+
+app.use(bodyParser())
 // use router middleware
 app.use(router.routes())
 
