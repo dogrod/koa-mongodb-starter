787d23fd962da2fefcf4457af0e1a5e73f460d44
zhulei <zhulei@xkeshi.com>
2017-07-19 11:40:53 +0800

    feat: fix mongoose warning

--- controllers/sign.js
+++ controllers/sign.js
@@ -2,36 +2,63 @@ var models  = require('../models')
 var User    = models.User
 
 exports.signup = async (ctx, next) => {
-  const username = ctx.request.body.username.toLowerCase()
-  const password = ctx.request.body.password
-  console.log(JSON.stringify(ctx))
+  // get request body
+  const bodyInfo = await ctx.request.body
 
-  User.find({ username }, (err, users) => {
-    if (err) {
-      return next(err)
+  const userInst = new User()
+
+  userInst.username = bodyInfo.username.toLowerCase()
+  userInst.password = bodyInfo.password
+
+  try {
+    const user = await userInst.save()
+
+    const res = {
+      code: 'success',
+      message: `${userInst.username}注册成功`
     }
-    if (users.length > 0) {
-      console.error('username exist')
-      ctx.body = {
-        code: 'failed',
-        message: '用户已存在'
-      }
-      return
-    }
 
-    let user = new User()
+    ctx.body = res
+    ctx.redirect(`/hello/${userInst.username}`)
 
-    user.username = username
-    user.password = password
+    return next()
+  } catch (error) {
+    ctx.body = error.toString()
+
+    return next()
+  }
+
+  // User.find({ username }, async (err, users) => {
+  //   if (err) {
+  //     return next(err)
+  //   }
+  //   if (users.length > 0) {
+  //     console.error('username exist')
+  //     ctx.body = {
+  //       code: 'failed',
+  //       message: '用户已存在'
+  //     }
+  //     return next()
+  //   }
+
+  //   let user = new User()
+
+  //   user.username = username
+  //   user.password = password
 
 
-    user.save((saveErr) => {
-      if (saveErr) {
-        console.error(JSON.stringify(saveErr))
-        return next(saveErr)
-      }
-      console.info('signup succeed')
-      ctx.redirect(`/hello/${username}`)
-    })
-  })
+  //   user.save(async (saveErr) => {
+  //     if (saveErr) {
+  //       console.error(JSON.stringify(saveErr))
+  //       return next(saveErr)
+  //     }
+  //     console.info('signup succeed')
+  //     ctx.body = {
+  //       code: 'success',
+  //       message: '注册成功'
+  //     }
+
+  //     return next()
+  //   })
+  // })
 }
--- models/index.js
+++ models/index.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 
 const config = require('../config')
 
+mongoose.Promise = global.Promise
 mongoose.connect(
   config.mongoDB, 
-  {}, 
+  { useMongoClient: true }, 
   (err) => {
     if (err) {
       console.log('connect mongoDB error:', err)
